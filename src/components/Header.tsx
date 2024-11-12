4bdfa2ece957262fdc1379d1de6cab5977a235a5
诸葛子房 <xiaoqiu2017wy@163.com>
2024-11-12 15:50:48 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -147,7 +147,7 @@ export default function Header({
           className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href={`/${locale}`} className="-m-1.5 p-1.5" onClick={() => setShowLoadingModal(true)}>
-              <Image className="h-8 w-auto" src="/appicon.svg" alt="Perfect Tidy" width={32} height={32}/>
+              <Image className="h-8 w-auto" src="/favicon.svg" alt="Perfect Tidy" width={32} height={32}/>
             </a>
             <a href={`/${locale}`} className="-m-1.5 ml-0.5 p-1.5" onClick={() => setShowLoadingModal(true)}>
               <Image
