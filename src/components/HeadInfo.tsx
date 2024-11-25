888642fadc33f415afc3da6c5b1558ab77f6eea7
诸葛子房 <xiaoqiu2017wy@163.com>
2024-11-25 09:23:38 +0800

    feat:ads

--- src/components/HeadInfo.tsx
+++ src/components/HeadInfo.tsx
@@ -9,11 +9,11 @@ const HeadInfo = ({
   return (
     <>
       <title>{title}</title>
+      <meta name="google-adsense-account" content="ca-pub-4158779929727637"/>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4158779929727637"
-           crossOrigin="anonymous"></script> 
-           <script async src='https://platform-api.sharethis.com/js/sharethis.js#property=6714ad2d10ca00001277ac7d&product=sop'>
-        
-        </script> 
+      crossOrigin="anonymous"></script> 
+      <script async src='https://platform-api.sharethis.com/js/sharethis.js#property=6714ad2d10ca00001277ac7d&product=sop'>
+      </script> 
       <meta name="description" content={description}/>
       {
         languages.map((item) => {
